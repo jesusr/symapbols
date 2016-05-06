9aa12b4635354695ab1eb1eb6c5b3bb76d3786c1
Jesús R Peinado <jesusrpeinado@gmail.com>
2016-05-06 23:33:30 +0200

    svg string retrieval function

--- symapbols.service.js
+++ symapbols.service.js
@@ -1,49 +1,57 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('symapbols')
-    .service('$symapbols', Service);
+    angular
+        .module('symapbols')
+        .service('$symapbols', Service);
 
-  function Service() {
-    this.opt = {};
-    this.symbols = {
-      'three-points': [{
-        'type': 'rect',
-        'x': '110',
-        'y': '220',
-        'width': '70',
-        'height': '70'
-      }, {
-        'type': 'rect',
-        'x': '110',
-        'y': '110',
-        'width': '70',
-        'height': '70'
-      }, {
-        'type': 'rect',
-        'x': '110',
-        'width': '70',
-        'height': '70'
-      }]
-    };
-
-
-    this.get = get;
-    this.setOptions = setOptions;
+    function Service() {
+        this.opt = {};
+        this.symbols = {
+            'three-points': [{
+                'type': 'rect',
+                'x': '110',
+                'y': '220',
+                'width': '70',
+                'height': '70'
+            }, {
+                'type': 'rect',
+                'x': '110',
+                'y': '110',
+                'width': '70',
+                'height': '70'
+            }, {
+                'type': 'rect',
+                'x': '110',
+                'width': '70',
+                'height': '70'
+            }]
+        };
 
 
-    // function setOptions(opt) {
+        this.svg = svg;
+        this.setOptions = setOptions;
 
-    // }
 
-    function svgElement(i) {
-      var data = symbols[i], svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      for (var i = data.length - 1; i >= 0; i--) {
-        data[i]
-      }
-      return svg;
+        // function setOptions(opt) {
+
+        // }
+
+        function svg(i) {
+            var internal = [],
+                data = symbols[i],
+                svg = '<svg width="1.2em" height="1.2em" fill="#000" viewBox="0 0 300 300">';
+            for (var i = data.length - 1; i >= 0; i--) {
+                svg += '<' + data[i].type;
+                svg += data[i].x ? ' x="' + data[i].x + '"' : '';
+                svg += data[i].y ? ' y="' + data[i].y + '"' : '';
+                svg += data[i].width ? ' width="' + data[i].width + '"' : '';
+                svg += data[i].height ? ' height="' + data[i].height + '"' : '';
+                svg += data[i].d ? ' d="' + data[i].d + '"' : '';
+                svg += '/>'
+            }
+            svg += '</svg>';
+            return svg;
+        }
     }
-  }
 })();
-
